63ed93fa9ec585c8e0a6428171b868962f0d11ca
truptigaonkar <trups11.trupti@gmail.com>
2020-07-31 22:10:56 +0200

    App component: styling with Styled components

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,67 @@
 import React from 'react';
-import './App.css';
+import styled, { createGlobalStyle } from "styled-components"
 import {BrowserRouter as Router, Route, NavLink, Switch} from 'react-router-dom'
 import Login from './components/Login';
 import Register from './components/Register';
 import Protected from './components/Protected';
 import Unprotected from './components/Unprotected';
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    text-align: center;
+    margin: 0px;
+    font-family: "Roboto";
+    background: #44c4e7;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`
+
+const Ul = styled.ul`
+  list-style-type:none;
+  font:bold 11px Helvetica,Arial,sans-serif;
+  text-transform:uppercase;
+  margin:0;
+  padding:0;
+`;
+
+const Li = styled.li`
+  display:inline-block;
+  background:#395870;
+  background:linear-gradient(#54a7e3,#103652);
+  padding:12px 20px;
+  :hover {
+    background:#1169b0;
+    box-shadow:inset 0 0  10px 1px rgba(0,0,0,.3);
+  }
+  :first-child a {
+    border-radius:4px 0 0 4px;
+  }
+  :last-child a {
+    border-radius:0 4px 4px 0;
+  }
+  .alink{
+    text-decoration: none;
+    color: #fff;
+    margin:10px
+  }
+  a.active-class{
+    color:#44c4e7;
+  }
+`;
+
 function App() {
   return (
-    <div className="App">
+    <>
+      <GlobalStyle />
       <Router>
-        <nav className="navigation">
-        <ul>
-          <li><NavLink className="alink" exact activeClassName='active-class' to="/">Login</NavLink></li>
-          <li><NavLink className="alink" exact activeClassName='active-class' to="/register">Register</NavLink></li>
-          <li><NavLink className="alink" exact activeClassName='active-class' to="/unprotected">Unprotected</NavLink></li>
-          <li><NavLink className="alink" exact activeClassName='active-class' to="/protected">Protected</NavLink></li>
-        </ul>
+        <nav>
+        <Ul>
+          <Li><NavLink className="alink" exact activeClassName='active-class' to="/">Login</NavLink></Li>
+          <Li><NavLink className="alink" exact activeClassName='active-class' to="/register">Register</NavLink></Li>
+          <Li><NavLink className="alink" exact activeClassName='active-class' to="/unprotected">Unprotected</NavLink></Li>
+          <Li><NavLink className="alink" exact activeClassName='active-class' to="/protected">Protected</NavLink></Li>
+        </Ul>
         </nav>
         <Switch>
           <Route exact path="/" component={Login} style={{ backgroundColor: '#44c4e7' }}/>
@@ -25,7 +70,7 @@ function App() {
           <Route path="/unprotected" component={Unprotected}/>
         </Switch>
       </Router>
-    </div>
+    </>
   );
 }
 
